90a6e5de0913fa268701b7fa15582fa7caddc9eb
otito <otitoamuga@gmail.com>
2025-06-06 16:00:56 -0400

    4th commit

---
+++ 01-linear-algebra/matrix_operations.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+# Matrix multiplication
+A = np.array([[1, 2], [3, 4]])
+B = np.array([[2, 0], [1, 2]])
+
+print(np.matmul(A, B))
---
+++ 01-linear-algebra/vectors_and_dot_product.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+a = np.array([1,2])
+b = np.array([3,4])
+
+# Dot product
+print(np.dot(a,b))
+
+
